1d2d9219075b2cbf3b5a86ccaf9fe49c6ebfe328
marcos10547 <160898439+marcos10547@users.noreply.github.com>
2025-11-26 19:16:42 +0100

    addCattegory

--- www/api.js
+++ www/api.js
@@ -2,7 +2,51 @@
 const API_BASE_URL = 'http://localhost:3000';
 
 class Api {
-    async function getAllCategories() {
-        const response = await fetch
+    async getAllCategories() {
+        const url = `${API_BASE_URL}/categories`;
+        
+        try {
+            const response = await fetch(url);
+            
+            if (!response.ok) {
+                const errorBody = await response.text(); 
+                throw new Error(`Error ${response.status}: ${errorBody || response.statusText}`);
+            }
+
+            return await response.json(); 
+
+        } catch (error) {
+            console.error("Error al obtener categorías:", error);
+            throw error; 
+        }
     }
+
+    async addCategory(categoryData) {
+        const url = `${API_BASE_URL}/categories`;
+        
+        const options = {
+            methods: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(categoryData)
+        };
+    
+        try {
+                const response = await fetch(url, options)
+
+                if (!options.ok) {
+                    const errorBody = await response.text(); 
+                    throw new Error(`Errooooor ${response.status}: ${errorBody || response.statusText}`)
+                }
+    
+                return await response.json();
+            
+        } catch (error) {
+            console.error("Error al crear categorías:", error);
+            throw error;
+        }
+    }
+    // addCategory, deleteCategory, findCategoryAndSites
+
 }
